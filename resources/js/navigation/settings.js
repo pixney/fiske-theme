8c971f60106427f0675f2e30cbe75b2f0106e9ca
William <williamastrom@gmail.com>
2017-09-06 11:14:35 +0200

    .

--- resources/js/navigation/settings.js
+++ resources/js/navigation/settings.js
@@ -1,17 +1,18 @@
 $(function () {
 
-console.log('#1');
+
     /**
      * Allow reordering the navigation.
      */
     $('#sidebar > ul').sortable({
         nested: false,
+         handle:'.handle',
         placeholder: '<li class="placeholder"/>',
         afterMove: function ($placeholder) {
             $placeholder.closest('ul').find('.dragged').detach().insertBefore($placeholder);
         },
         onDrop: function ($item, container, _super) {
-            console.log('#2');
+          
             var navigation = [];
 
             $('#sidebar > ul > li').each(function () {
